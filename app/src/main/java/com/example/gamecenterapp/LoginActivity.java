b38a6e17798c58cc019ec2817e837cb2afddcaf2
Celeste Duguay <cdugu093@uottawa.ca>
2022-09-16 11:11:17 -0400

    added link to sign up activity from login activity.

--- app/src/main/java/com/example/gamecenterapp/LoginActivity.java
+++ app/src/main/java/com/example/gamecenterapp/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.gamecenterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,6 +38,12 @@ public class LoginActivity extends AppCompatActivity {
 
                 }
 
+            }
+        });
+
+        signupNowBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
 
