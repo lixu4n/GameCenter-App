41608e409f58d603c1bbe5b7f81da427b0d2c2f1
uyennguyen00 <Uyenut15!>
2022-09-16 11:15:56 -0400

    fixed signup's look

--- app/src/main/java/com/example/gamecenterapp/SignUpActivity.java
+++ app/src/main/java/com/example/gamecenterapp/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.gamecenterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        final EditText fullName = findViewById( R.id.fullName);
+        final EditText phoneNumber = findViewById( R.id.phoneNum);
+        final EditText emailAdd = findViewById( R.id.emailAddress);
+        final EditText password = findViewById( R.id.password);
+        final EditText  confirmPassword = findViewById( R.id.confirmPassword);
+
+
     }
 }
